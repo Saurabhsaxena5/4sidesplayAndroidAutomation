f87eade6aa920774733a36d2bcd829b72d03f1ed
Saurabh Saxena <saurabhsaxena5@gmail.com>
2025-08-11 13:28:58 +0530

    LoginTestcase

--- src/test/java/Appium/Appium/Demo.java
+++ src/test/java/Appium/Appium/Demo.java
@@ -1,55 +1,51 @@
 package Appium.Appium;
 
-import java.net.MalformedURLException;
 import java.net.URL;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import io.appium.java_client.android.AndroidDriver;
 
 public class Demo {
 
-	AndroidDriver driver;
+    private AndroidDriver driver;
 
-	@BeforeTest
-	public void setup() {
+    @BeforeTest
+    public void setup() {
+        try {
+            DesiredCapabilities dc = new DesiredCapabilities();
 
-		DesiredCapabilities dc = new DesiredCapabilities();
-		try {
+            // ✅ Appium 2.x W3C format
+            dc.setCapability("platformName", "Android");
+            dc.setCapability("appium:deviceName", "Xiaomi 23028RN4DI API 33");
+            dc.setCapability("appium:automationName", "uiautomator2");
+            dc.setCapability("appium:app", "C:\\Users\\Yash\\.appium\\4isesplay.apk");
 
-			// ✅ Use correct Appium 2 + W3C capability format
-			dc.setCapability("platformName", "Android");
-			dc.setCapability("platformName", "Android");
+            // ✅ Proper server URL
+            URL url = new URL("http://127.0.0.1:4723/wd/hub");
 
-			dc.setCapability("appium:deviceName", "Xiaomi 23028RN4DI");
-			dc.setCapability("appium:automationName", "uiautomator2");
-			dc.setCapability("appium:app", "C:\\Users\\Yash\\.appium\\4isesplay.apk");
+            // ✅ Assign to class variable, not local
+            driver = new AndroidDriver(url, dc);
+            System.out.println("Session started successfully.");
 
-			// ✅ Proper server URL
-			URL url = new URL("http://127.0.0.1:4723/");
+        } catch (Exception e) {
+            System.out.println("Setup failed: " + e.getMessage());
+        }
+   
 
-			AndroidDriver driver = new AndroidDriver(url, dc);
-			System.out.println("Session started successfully.");
-		} catch (Exception e) {
-			System.out.println("If it is failed so :" + e.getMessage());
-		}
-	}
+    @Test
+    public void clickonbanner() {
+        driver.findElement(By.xpath(
+            "//android.widget.ScrollView/android.view.View[1]/android.view.View/android.widget.ImageView[2]"
+        )).click();
+    }
 
-	@Test
-	public void clickonbanner() {
-		driver.findElement(By.xpath("//android.widget.ScrollView/android.view.View[1]/android.view.View/android.widget.ImageView[2]")).click();
-		
-	}
-	
-	
-	@AfterTest
-	public void Teardorn() {
-
-		driver.quit();
-	}
+    @AfterTest
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
